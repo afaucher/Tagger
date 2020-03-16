e62a9a940005c2c36dcaf3109a17875c61d3de2b
Alexander Faucher <alexanderfaucher@gmail.com>
2020-03-15 19:56:01 -0700

    Add ammo indicator LEDs.

--- Tagger.ino
+++ Tagger.ino
@@ -1,9 +1,9 @@
 /*
- * Based on LazerTagHost by Alex Faucher
- * https://github.com/afaucher/LazerTagHost
- * Based on IRremote by Ken Shirriff
- * http://arcfn.com
- */
+   Based on LazerTagHost by Alex Faucher
+   https://github.com/afaucher/LazerTagHost
+   Based on IRremote by Ken Shirriff
+   http://arcfn.com
+*/
 
 #include <IRremote.h>
 #include <FastLED.h>
@@ -15,9 +15,10 @@
 // LED Indicators
 // ==============
 
-#define NUM_LEDS 8
-#define LED_PIN 6
-CRGB leds[NUM_LEDS];
+#define NUM_LEDS_PER_STRIP 8
+#define HEALTH_LED_PIN 6
+#define AMMO_LED_PIN 7
+CRGB leds[NUM_LEDS_PER_STRIP * 2];
 
 // ==============
 // IR State
@@ -47,7 +48,7 @@ decode_results results;
 
 struct RcReceiver {
   uint32_t rc_update;
-  float RC_in[NUMBER_RC_CHANNELS];                    // an array to store the calibrated input from receiver 
+  float RC_in[NUMBER_RC_CHANNELS];                    // an array to store the calibrated input from receiver
 };
 
 RcReceiver rcReceiver = {};
@@ -125,7 +126,7 @@ void dump(decode_results *results) {
   uint8_t count = results->rawlen;
   if (results->decode_type == UNKNOWN) {
     Serial.println(F("Could not decode message"));
-  } 
+  }
   else {
     if (results->decode_type == PHOENIX_LTX) {
       Serial.print(F("LTX: "));
@@ -144,7 +145,7 @@ void dump(decode_results *results) {
   for (uint8_t i = 0; i < count; i++) {
     if ((i % 2) == 1) {
       Serial.print(results->rawbuf[i]*USECPERTICK, DEC);
-    } 
+    }
     else {
       Serial.print(-(int)results->rawbuf[i]*USECPERTICK, DEC);
     }
@@ -158,13 +159,13 @@ void setup()
   //Start IR TX
   pinMode(IR_TX_PIN, OUTPUT);
   digitalWrite(IR_TX_PIN, LOW);
-  
+
   // Setup Button Input
   buttons.hitButton.attach(HIT_BUTTON_PIN, INPUT_PULLUP);
   buttons.hitButton.interval(DEBOUNCE_INTERVAL_MS);
   buttons.triggerButton.attach(TRIGGER_BUTTON_PIN, INPUT_PULLUP);
   buttons.triggerButton.interval(DEBOUNCE_INTERVAL_MS);
-  
+
   // Start the USB Serial
   Serial.begin(115200);
   Serial.println(F("Start"));
@@ -173,11 +174,14 @@ void setup()
   // Start the PWM RC receiver
   setup_pwmRead();
   // Setup LEDs
-  FastLED.addLeds<NEOPIXEL, LED_PIN>(leds, NUM_LEDS);
+  // Note: The decision to seperate these was based on the wiring being easier.  This can be
+  // refactored to support one long continous strip if needed.
+  FastLED.addLeds<NEOPIXEL, HEALTH_LED_PIN>(leds, 0, NUM_LEDS_PER_STRIP);
+  FastLED.addLeds<NEOPIXEL, AMMO_LED_PIN>(leds, NUM_LEDS_PER_STRIP, NUM_LEDS_PER_STRIP);
   setupLedValuesNow();
-  //Duemilanove 5v regulator is only rated to 500mA on USB, which also powers the board.
-  //Keep a conservative limit here.
-  FastLED.setMaxPowerInVoltsAndMilliamps(5, 100); 
+  // Duemilanove 5v regulator is only rated to 500mA on USB, which also powers the board.
+  // Keep a conservative limit here.  Adjust if you know your board can drive higher current.
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 100);
   updateLedValuesNow();
 }
 
@@ -185,26 +189,24 @@ void shoot(uint16_t team, uint16_t player, uint16_t damage) {
   //Damage is encoded 0-3 for damage range [1-4]
   damage--;
   uint16_t data = ((team & 0x03) << 5)
-      | ((player & 0x07) << 2)
-      | (damage & 0x02);
+                  | ((player & 0x07) << 2)
+                  | (damage & 0x02);
   irsend.sendPHOENIX_LTX(data, 7);
   irrecv.enableIRIn();
   irrecv.resume();
 }
 
-//Manages led state
+//Update the LED state from the current game state
+//Does not refresh the LEDs
 void updateLedValues() {
-  uint8_t healthPerLed = MAX_LIFE / NUM_LEDS;
-  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+  //HEALTH STRIP
+  uint8_t healthPerLed = MAX_LIFE / NUM_LEDS_PER_STRIP;
+  for (uint8_t i = 0; i < NUM_LEDS_PER_STRIP; i++) {
     //The order here defines precidence for which rule controls the output
     if (gameState.life == 0) { //Player Death
       leds[i] = CRGB::White;
     } else if (gameState.hitCountdown > 0) { //Hit Indicator
       leds[i] = CRGB::Red;
-    } else if (gameState.lastShotFiredTimeMillis > 0 && gameState.ammo > 0) { //Shot Indicator
-        //Give an indication while firing
-        //TODO - This should move over to the 'ammo' LED once we define that
-        leds[i] = CRGB(0,0,100);
     } else { //Show Health
       if (i * healthPerLed < gameState.life) {
         //Note: Partial health can be indicated by blinking the last element
@@ -214,10 +216,32 @@ void updateLedValues() {
       }
     }
   }
+  //AMMO STRIP
+  uint8_t ammoPerLed = MAX_AMMO / NUM_LEDS_PER_STRIP;
+  for (uint8_t i = NUM_LEDS_PER_STRIP; i < NUM_LEDS_PER_STRIP * 2; i++) {
+    //The order here defines precidence for which rule controls the output
+    if (gameState.life == 0) { //Player Death
+      leds[i] = CRGB::White;
+    } else if (gameState.hitCountdown > 0) { //Hit Indicator
+      leds[i] = CRGB::Red;
+    } else { //Show Ammo & Shot indicator
+      if ((i - NUM_LEDS_PER_STRIP) * ammoPerLed < gameState.ammo) {
+        if (gameState.lastShotFiredTimeMillis > 0 && gameState.ammo > 0) { //Shot Indicator
+          //Give an indication while firing by making ammo orange
+          leds[i] = CRGB::Orange;
+        } else { //Ammo indicator
+          leds[i] = CRGB::Blue;
+        }
+      } else {
+        leds[i] = CRGB::Black;
+      }
+    }
+  }
 }
 
+//Initialize LEDs to a neutral value to diagnose any initialization issues
 void setupLedValuesNow() {
-  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+  for (uint8_t i = 0; i < NUM_LEDS_PER_STRIP * 2; i++) {
     leds[i] = CRGB::Grey;
   }
   FastLED.show();
@@ -292,61 +316,60 @@ void updateGame(uint32_t now) {
 void readIrInput() {
   if (irrecv.decode(&results)) {
     //dump(&results);
-    
+
     //We only understand shots for now and we can just assume everything is all right
-     if (results.decode_type == PHOENIX_LTX && results.bits == 7) {
-       //https://executedata.blogspot.com/2010/09/lazertag-team-ops-packet-part-1.html
-       //[ 3 bits - zero ] [ 2 bits - team ] [ 2 bits - damage ]
-       // Team:
-       // 0x00 = Solo
-       // 0x01-0x03 = Team 1-3
-       // Damage:
-       // 0x00-0x03 = Damage 1-4 (3-4 are only usable by LTTO as Mega-Tag)
-       uint8_t team = (results.value >> 2) & 0x3;
-       uint8_t damage = (results.value >> 0) & 0x3;
-       //TODO - Clean up team detection
-       if (team == 0 && gameState.teamId == 0) {
-         //Solo
-         inputState.hit |= true;
-         inputState.hitDamage = damage + 1;
-       } else if (team != 0 && gameState.teamId != 0 && team != gameState.teamId) {
-         //We are not playing solo and not on the same team
-         inputState.hit |= true;
-         inputState.hitDamage = damage + 1;
-       }
-     } else {
-       stats.unknownIrPacket++;
-     }
-    
+    if (results.decode_type == PHOENIX_LTX && results.bits == 7) {
+      //https://executedata.blogspot.com/2010/09/lazertag-team-ops-packet-part-1.html
+      //[ 3 bits - zero ] [ 2 bits - team ] [ 2 bits - damage ]
+      // Team:
+      // 0x00 = Solo
+      // 0x01-0x03 = Team 1-3
+      // Damage:
+      // 0x00-0x03 = Damage 1-4 (3-4 are only usable by LTTO as Mega-Tag)
+      uint8_t team = (results.value >> 2) & 0x3;
+      uint8_t damage = (results.value >> 0) & 0x3;
+      //TODO - Clean up team detection
+      if (team == 0 && gameState.teamId == 0) {
+        //Solo
+        inputState.hit |= true;
+        inputState.hitDamage = damage + 1;
+      } else if (team != 0 && gameState.teamId != 0 && team != gameState.teamId) {
+        //We are not playing solo and not on the same team
+        inputState.hit |= true;
+        inputState.hitDamage = damage + 1;
+      }
+    } else {
+      stats.unknownIrPacket++;
+    }
+
     irrecv.enableIRIn();
     //Serial.print(F("Value: "));
     //Serial.println(results.value, BIN);
-    
+
     irrecv.resume(); // Receive the next value
-    
-     
+
+
   }
 }
 
 void readRcInput(uint32_t now) {
-  if(RC_avail() || now - rcReceiver.rc_update > RECEIVER_UPDATE_PERIOD_MILLIS) {   // if RC data is available or 25ms has passed since last update (adjust to be equal or greater than the frame rate of receiver)
-      
-      rcReceiver.rc_update = now;                           
-      
-      //Note: This printing can break uploading somehow?  Uncommenting it causes upload to fail.
-      //print_RCpwm();                        // uncommment to print raw data from receiver to serial
-      
-      for (uint8_t i = 0; i < NUMBER_RC_CHANNELS; i++){       // run through each RC channel
-        uint8_t ch = i + 1;
-        
-        rcReceiver.RC_in[i] = RC_decode(ch);             // decode receiver channel and apply failsafe
-        
-        //Serial.println(rcReceiver.RC_in[i]);   // uncomment to print calibrated receiver input (+-100%) to serial       
-      }
-      //Serial.println();                       // uncomment when printing calibrated receiver input to serial.
+  if (RC_avail() || now - rcReceiver.rc_update > RECEIVER_UPDATE_PERIOD_MILLIS) {  // if RC data is available or 25ms has passed since last update (adjust to be equal or greater than the frame rate of receiver)
+
+    rcReceiver.rc_update = now;
+
+    //print_RCpwm();                        // uncommment to print raw data from receiver to serial
+
+    for (uint8_t i = 0; i < NUMBER_RC_CHANNELS; i++) {      // run through each RC channel
+      uint8_t ch = i + 1;
+
+      rcReceiver.RC_in[i] = RC_decode(ch);             // decode receiver channel and apply failsafe
+
+      //Serial.println(rcReceiver.RC_in[i]);   // uncomment to print calibrated receiver input (+-100%) to serial
     }
-    inputState.triggerPulled |= TRIGGER_RANGE_VALUE - RECEIVER_TRIGGER_TOLERANCE_PERCENT < rcReceiver.RC_in[TRIGGER_CHANNEL]
-        && rcReceiver.RC_in[TRIGGER_CHANNEL] < TRIGGER_RANGE_VALUE + RECEIVER_TRIGGER_TOLERANCE_PERCENT;
+    //Serial.println();                       // uncomment when printing calibrated receiver input to serial.
+  }
+  inputState.triggerPulled |= TRIGGER_RANGE_VALUE - RECEIVER_TRIGGER_TOLERANCE_PERCENT < rcReceiver.RC_in[TRIGGER_CHANNEL]
+                              && rcReceiver.RC_in[TRIGGER_CHANNEL] < TRIGGER_RANGE_VALUE + RECEIVER_TRIGGER_TOLERANCE_PERCENT;
 }
 
 void loop() {
